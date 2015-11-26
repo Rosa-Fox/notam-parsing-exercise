a963cbd6e5e1a7b65b39c26bb3930456098e5549
Rosa-Fox <rosafox89@gmail.com>
2015-11-26 12:34:40 +0000

    Separates out each notam and gives days method to access the text we want to display

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,41 @@
 class ResultsController < ApplicationController
   def index
+    notams = []
+    remove_lines
+    paragraph
+    File.open('public/notam.txt', 'r') do |f|
+      f.each_line do |line|
+        if line.include? 'AERODROME HOURS OF OPS/SERVICE'
+          notams << line
+        end
+      end
+      @notams = notams
+    end
+    @days = days
+  end
+
+  def remove_lines
+    text = File.read('public/notam.txt')
+    new_contents = text.gsub(/\n/, '')
+    File.open('public/notam.txt', 'w') { |file| file.puts new_contents }
+  end
+
+  def paragraph
+    text = File.read('public/notam.txt')
+    new_contents = text.gsub(/SOURCE: EUECYIYN/, "SOURCE: EUECYIYN" + "\n")
+    File.open('public/notam.txt', 'w') { |file| file.puts new_contents }
+  end
+
+  def days
+    {
+      'mon' => ['MON-', 'MON'],
+      'tue' => ['TUE-', 'TUE', 'MON-'],
+      'wed' => ['WED-', 'WED', 'TUE-', 'MON-'],
+      'thu' => ['THU-', 'THU', 'WED-"', 'TUE-', 'MON-'],
+      'fri' => ['FRI'],
+      'sat' => ['SAT'],
+      'sun' => ['SUN']
+    }
   end
 
   def notam
