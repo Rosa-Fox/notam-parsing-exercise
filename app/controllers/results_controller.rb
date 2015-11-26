ae316179095c9b33fe4adfab05f45ed68124defd
Rosa-Fox <rosafox89@gmail.com>
2015-11-26 12:00:18 +0000

    Added notam method to write the notam data from form submission into a text file

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,4 +1,13 @@
 class ResultsController < ApplicationController
   def index
   end
+
+  def notam
+    notam = params[:notam]
+    path = 'public/notam.txt'
+    File.open(path, 'w+') do |f|
+      f.write(notam)
+    end
+    redirect_to results_url
+  end
 end
